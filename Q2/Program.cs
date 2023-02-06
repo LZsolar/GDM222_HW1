ad6bc320040f97790a6d0e12ff012e00d860b86c
LZsolar <minecraftradioevil@gmail.com>
2023-02-06 09:32:02 +0700

    Q3

---
+++ Q2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double X1 = double.Parse(Console.ReadLine());
+        double Y1 = double.Parse(Console.ReadLine());
+        double X2 = double.Parse(Console.ReadLine());
+        double Y2 = double.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        f1(n,X1,Y1,X2,Y2);
+    }
+    static void f1(int n,double X1,double Y1,double X2,double Y2){
+        if(n==0){return;}
+        
+        X2 = X2-(X2-X1)-(X2-X1);
+        X1 = (X1-X2)/2;
+        Y1 = (Y2-Y1)/2;
+        Console.WriteLine(X1);
+        Console.WriteLine(Y1);
+
+        f2(n-1,X1,X2,Y1,Y2);
+    }
+    static void f2(int n,double X1,double Y1,double X2,double Y2){
+        if(n==0){return;}
+        
+        X2 = X2- (X2-X1);
+    }
+    static void f3(int n,double X1,double Y1,double X2,double Y2){
+        if(n==0){return;}
+        
+        X2 = X2- (X2-X1);
+    }
+    static void f4(int n,double X1,double Y1,double X2,double Y2){
+        if(n==0){return;}
+        
+        X2 = X2- (X2-X1);
+    }
+    
+}
